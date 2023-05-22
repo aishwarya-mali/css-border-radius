04a267ddfad347bd7e6dc122ab7d9ed6ca8086a6
aishwarya-mali <aishwaryamali29@gmail.com>
2023-05-22 13:09:44 +0530

    refactored code

--- js/script.js
+++ js/script.js
@@ -9,90 +9,106 @@ const code = document.querySelector('.code-area code')
 const errorMessages = document.querySelectorAll('.errorMessage')
 const inputs = document.querySelectorAll('.input')
 const pattern = /^[0-9]+(px|%|rem|em)$/
-let tl = '';
-let tr = '';
-let bl = '';
-let br = '';
+let topLeftValue = '';
+let topRightValue = '';
+let bottomLeftValue = '';
+let bottomRightValue = '';
 
-topLeft.addEventListener('input', function (e) {
-  const isValid = validateInput(this, 'tl')
-  if (!isValid) return;
-  tl = e.target.value
-  main.style.borderTopLeftRadius = tl
-  updateCode(tl, tr, br, bl)
-})
-
-topRight.addEventListener('input', function (e) {
-  const isValid = validateInput(this, 'tr')
-  if (!isValid) return;
-  tr = e.target.value
-  main.style.borderTopRightRadius = tr
-  updateCode(tl, tr, br, bl)
-})
-
-bottomRight.addEventListener('input', function (e) {
-  const isValid = validateInput(this, 'br')
-  if (!isValid) return;
-  br = e.target.value
-  main.style.borderBottomRightRadius = br
-  updateCode(tl, tr, br, bl)
-})
-
-bottomLeft.addEventListener('input', function (e) {
-  const isValid = validateInput(this, 'bl')
-  if (!isValid) return;
-  bl = e.target.value
-  main.style.borderBottomLeftRadius = bl
-  updateCode(tl, tr, br, bl)
-})
-
-function validateInput(input, dir) {
+function validateInput(input, errorMessage) {
   const isValid = pattern.test(input.value);
-  const errorMessage = document.getElementById(dir);
   if (isValid) {
-    input.style.borderColor = 'initial'
+    input.style.borderColor = 'initial';
     errorMessage.style.display = "none";
     return true;
   } else {
-    input.style.borderColor = 'red'
+    input.style.borderColor = 'red';
     errorMessage.style.display = "block";
     return false;
   }
 }
 
+function updateBoxStyles(tl, tr, br, bl) {
+  main.style.borderTopLeftRadius = tl;
+  main.style.borderTopRightRadius = tr;
+  main.style.borderBottomRightRadius = br;
+  main.style.borderBottomLeftRadius = bl;
+  updateCode(tl, tr, br, bl);
+}
+
 function updateCode(lt, rt, rb, lb) {
   code.innerHTML = ''
+  const value = `${lt} ${rt} ${rb} ${lb}`
   const html =
     `
-    border-radius: ${tl} ${tr} ${br} ${bl}; <br/>
-    -moz-border-radius: ${tl} ${tr} ${br} ${bl}; <br/>
-    -webkit-border-radius: ${tl} ${tr} ${br} ${bl};  <br/>
+    border-radius: ${value}; <br/>
+    -moz-border-radius: ${value}; <br/>
+    -webkit-border-radius: ${value};  <br/>
   `
   code.insertAdjacentHTML('afterbegin', html)
 }
 
-btnReset.addEventListener('click', function(e){
+function resetValues() {
   topLeft.value = topRight.value = bottomLeft.value = bottomRight.value = ''
   errorMessages.forEach(err => {
-    err.style.display = "none";
+    err.style.display = "none"
   })
   inputs.forEach(input => {
     input.style.borderColor = 'initial'
   })
-})
+}
 
-btnCopy.addEventListener('click', function(){
-  const text = code.textContent;
+function copyCode() {
+  const text = code.textContent
   navigator.clipboard.writeText(text)
     .then(() => {
-      code.style.backgroundColor = '#FF8A65';
+      code.style.backgroundColor = '#FF8A65'
       code.style.color = '#333333'
       setTimeout(() => {
-        code.style.backgroundColor = ''; 
-        code.style.color = '';
-      }, 1000);
+        code.style.backgroundColor = ''
+        code.style.color = ''
+      }, 1000)
     })
     .catch((err) => {
-      console.error('Failed to copy text: ', err);
-    });
+      console.error('Failed to copy text: ', err)
+    })
+}
+
+topLeft.addEventListener('input', function (e) {
+  const errorMessage = document.getElementById('tl');
+  const isValid = validateInput(this, errorMessage);
+  if (!isValid) return;
+  topLeftValue = e.target.value
+  updateBoxStyles(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
+  updateCode(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
 })
+
+topRight.addEventListener('input', function (e) {
+  const errorMessage = document.getElementById('tr');
+  const isValid = validateInput(this, errorMessage);
+  if (!isValid) return;
+  topRightValue = e.target.value
+  updateBoxStyles(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
+  updateCode(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
+})
+
+bottomRight.addEventListener('input', function (e) {
+  const errorMessage = document.getElementById('br');
+  const isValid = validateInput(this, errorMessage);
+  if (!isValid) return;
+  bottomRightValue = e.target.value
+  updateBoxStyles(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
+  updateCode(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
+})
+
+bottomLeft.addEventListener('input', function (e) {
+  const errorMessage = document.getElementById('bl');
+  const isValid = validateInput(this, errorMessage);
+  if (!isValid) return;
+  bottomLeftValue = e.target.value
+  updateBoxStyles(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
+  updateCode(topLeftValue, topRightValue, bottomRightValue, bottomLeftValue)
+})
+
+btnReset.addEventListener('click', resetValues)
+
+btnCopy.addEventListener('click', copyCode)
